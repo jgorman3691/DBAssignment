fd33928414424def56647dd0ae2b823fdac399a6
Jed Gorman <jed.gorman@gmail.com>
2020-07-22 10:21:50 -0400

    Still Reacquiring Skills

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+
+
+echo(' <!DOCTYPE html>');
+echo('  <head>');
+echo('   <div class="head" id="top">');
+echo('     <meta name="author" content="Jed Gorman" />');
+echo('     <meta name="keywords" content="HTML, CSS,PHP, JavaScript, and MariaDB using SQL" />');
+echo('     <meta name="description" content="Database Assignment" />');
+echo('     <meta name="viewport" content="width=device.width, initil-scale=1.0" />');
+echo('     <meta charset="UTF-8" />');
+echo('     <link rel="stylesheet" ref="includes/CSS/mainstyle.css" />');
+echo('     <header>');
+echo('       <title>Just the Skeleton</title>');
+echo('     </header>');
+echo('  </head>');
+echo('  <a href="/data-entry.php" id="DBEntry">Database Entry</a>')
+
+
+?>
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+include 'header.php';
+
+?>
+<body>
+  <h1>Just the Index Test!</h1>
+  <p>This is just a test paragraph to see what is going on in this.</p>
+  <br />
+  <br />
+  <p>These are the items we need to be able to insert into the databases, and order them based on what table is chosen.<br />
+  What is unfortunate is that we're going to have to create a Global ID for each tenant.</p>
+  <ul>
+    <li>Create Database</li>
+    <li>Create GlobalID</li>
+    <li>Create Table</li>
+    <li>Create User</li>
+    <li>Create Column</li>
+    <li>Create Row</li>
+    <li>Create Key</li>
+    <li>Create Attributes</li>
+    <li>Create ForeignKey</li>
+    <li>Create Index</li>
+    <li>Alter Database</li>
+    <li>Alter Table</li>
+    <li>Alter Column</li>
+    <li>Alter Row</li>
+    <li>Alter Attributes</li>
+    <li>Alter ForeignKey</li>
+    <li>Alter User</li>
+    <li>Alter Key</li>
+    <li>Alter Index</li>
+    <li>Drop Key</li>
+    <li>Drop User</li>
+    <li>Drop Column</li>
+    <li>Drop Row</li>
+    <li>Drop Table</li>
+    <li>Drop Database</li>
+  </ul>
+
+</body>
+<footer>
+</footer>
+</html>
