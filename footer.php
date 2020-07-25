6b607d845916fff36682af4eba4424dc4051ad6f
Jed Gorman <jed.gorman@gmail.com>
2020-07-24 23:46:22 -0400

    Just a Mess of a new page

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+
+echo('  <footer>');
+echo('    <p>Author: Jed Gorman</p>');
+echo('    <br />');
+echo('    <br />');
+echo('    <p>Assigned by: Dan Hyatt</p>');
+echo('    <br />');
+echo('    <br />');
+echo('    <p>If you wish to e-mail me, please do so <a href="mailto:jed.gorman@gmail.com">here.</a></p>');
+echo('    <br />');
+echo('    <br />');
+echo('  </footer>');
+echo('</html>');
+
+?>
